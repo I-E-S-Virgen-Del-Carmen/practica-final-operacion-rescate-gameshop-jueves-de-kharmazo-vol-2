6989914e3416051bf639fbb2271f45f9f3f0ba62
Adrian <amoyvil1909@g.educaand.es>
2026-02-20 00:43:37 +0100

    Docs: Añadido JavaDoc a GestorInventario

--- src/main/java/com/gameshop/GestorInventario.java
+++ src/main/java/com/gameshop/GestorInventario.java
@@ -17,10 +17,25 @@ public class GestorInventario {
         System.out.println("Total de juegos en el almacén: " + totalJuegos);
     }
 
+    /**
+     * Calcula el total de juegos
+     *
+     * @param totalJuegos Total de juegos actuales
+     * @param copiasDisponibles Numero de copias actuales
+     * @return El nuevo total de juegos sumando las copias
+     */
+
     private static int getTotalJuegos(int totalJuegos, int copiasDisponibles) {
         return totalJuegos + copiasDisponibles;
     }
 
+    /**
+     * Verifica el stock de un juego y manda una alerta si el stock es menor de 5.
+     *
+     * @param nombreJuegos Array con los nombres de los juegos
+     * @param copiasDisponibles Array con todas las copias de cada juego
+     * @param i posicion del juego que hemos pedido analizar
+     */
     private static void obtenerStock(String[] nombreJuegos, int[] copiasDisponibles, int i) {
         if (copiasDisponibles[i] <= LIMITE_STOCK) {
             System.out.println("¡ALERTA! Pedir más copias de " + nombreJuegos[i]);
